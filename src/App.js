052fa070c9a1cfa0bb5d8b25179a3c2c19e618a9
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-06-21 18:46:51 -0300

    creates physical keyboard functionality

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
-  const [currentAttempt, setCurrentAttempt] = useState({attempt: 0, letterPos: 0});
+  const [currentAttempt, setCurrentAttempt] = useState({
+    attempt: 0,
+    letterPos: 0,
+  });
+
+  const correctWord = "RIGHT";
 
   const onSelectLetter = (keyValue) => {
     if (currentAttempt.letterPos > 4) return;
@@ -17,20 +22,23 @@ function App() {
     setBoard(newBoard);
     setCurrentAttempt({
       ...currentAttempt,
-      letterPos: currentAttempt.letterPos + 1
+      letterPos: currentAttempt.letterPos + 1,
     });
   };
 
   const onDelete = () => {
     if (currentAttempt.letterPos === 0) return;
-      const newBoard = [...board];
-      newBoard[currentAttempt.attempt][currentAttempt.letterPos -1] = "";
-      setBoard(newBoard);
-      setCurrentAttempt({...currentAttempt, letterPos: currentAttempt.letterPos -1})
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrentAttempt({
+      ...currentAttempt,
+      letterPos: currentAttempt.letterPos - 1,
+    });
   };
 
   const onEnter = () => {
-    if (currentAttempt.currentPos !== 5) return;
+    if (currentAttempt.letterPos !== 5) return;
 
     setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPos: 0 });
   };
@@ -39,7 +47,18 @@ function App() {
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <AppContext.Provider value={{ board, setBoard, currentAttempt, setCurrentAttempt, onSelectLetter, onDelete, onEnter }}>
+      <AppContext.Provider
+        value={{
+          board,
+          setBoard,
+          currentAttempt,
+          setCurrentAttempt,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+          correctWord,
+        }}
+      >
         <div className="game">
           <Board />
           <Keyboard />
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,33 +1,70 @@
-import React from 'react';
-import Key from './Key';
+import React, { useContext, useEffect, useCallback } from "react";
+import { AppContext } from "../App";
+import Key from "./Key";
 
 const Keyboard = () => {
-    const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
-    const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-    const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+  const { onEnter, onDelete, onSelectLetter } = useContext(AppContext);
 
-    return (
-        <div className='keyboard'>
-            <div className='line1'>
-                {keys1.map((key) => {
-                    return <Key keyValue={key}/>
-                })}
-            </div>
-            <div className='line2'>
-            {keys2.map((key) => {
-                    return <Key keyValue={key}/>
-                })}
-            </div>
-            <div className='line3'>
-            <Key keyValue={"ENTER"} bigKey />
-            {keys3.map((key) => {
-                return <Key keyValue={key}/>
-            })}
-            <Key keyValue={"DELETE"} bigKey />
+  const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
 
-            </div>
-        </div>
-    )
+//useCallback prevents re-updating the component
+  const handleKeyboard = useCallback((event) => {
+    if (event.key === "Enter") {
+      onEnter();
+    } else if (event.key === "Backspace") {
+      onDelete();
+    } else {
+      keys1.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+
+      keys2.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+
+      keys3.forEach((key) => {
+        if (event.key.toLowerCase() === key.toLowerCase()) {
+          onSelectLetter(key);
+        }
+      });
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboard);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    };
+  }, [handleKeyboard]);
+
+  return (
+    <div className="keyboard" onKeyDown={handleKeyboard}>
+      <div className="line1">
+        {keys1.map((key) => {
+          return <Key keyValue={key} />;
+        })}
+      </div>
+      <div className="line2">
+        {keys2.map((key) => {
+          return <Key keyValue={key} />;
+        })}
+      </div>
+      <div className="line3">
+        <Key keyValue={"ENTER"} bigKey />
+        {keys3.map((key) => {
+          return <Key keyValue={key} />;
+        })}
+        <Key keyValue={"DELETE"} bigKey />
+      </div>
+    </div>
+  );
 };
 
 export default Keyboard;
