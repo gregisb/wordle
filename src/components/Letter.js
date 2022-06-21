05b7fdb2ed5f411cc1d1ad9d6de2634785fd9a6f
Guilherme Regis <guilhermeb.regis@gmail.com>
2022-06-21 19:56:00 -0300

    creates disabled letters logic

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,8 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AppContext } from "../App";
 
 const Letter = ({ letterPos, attemptValue }) => {
-  const { board, correctWord, currentAttempt } = useContext(AppContext);
+  const { board, correctWord, currentAttempt, disabledLetters, setDisabledLetters } = useContext(AppContext);
   const letter = board[attemptValue][letterPos];
 
   const correct = correctWord[letterPos] === letter;
@@ -11,6 +11,12 @@ const Letter = ({ letterPos, attemptValue }) => {
   const letterState =
     currentAttempt.attempt > attemptValue &&
     (correct ? "correct" : almost ? "almost" : "error");
+
+    useEffect(() => {
+      if (letter !== "" && !correct && !almost) {
+        setDisabledLetters([...disabledLetters, letter])
+      }
+    }, [currentAttempt.attempt])
   return (
     <div className="letter" id={letterState}>
       {letter}
